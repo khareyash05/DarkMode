5cce76d62572f8cae24425a47a003b024e1f4375
Yash Khare <khareyash05@gmail.com>
2021-02-24 23:18:52 +0530

    commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const options = {
     buttonColorDark: '#100f2c',  // default: '#100f2c'
     buttonColorLight: '#fff', // default: '#fff'
     saveInCookies: true, // default: true,
-    label: '😂', // default: ''
+    label: '🌓', // default: ''
     autoMatchOsTheme: true // default: true
 }
   
